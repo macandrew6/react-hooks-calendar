f8d06ffbd4ecca3ee6ca110263acd6c068c46ca4
macandrew6 <mr.betterandrewmac@gmail.com>
2021-03-25 16:17:04 -0700

    use moment to find start day and end day of each month

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -3,6 +3,13 @@ import moment from 'moment';
 
 export default function Calendar() {
   const value = moment();
-  const startDay = value.clone().startOf('month');
-  return <div>{startDay.format('MM/DD/YY')}</div>;
+  const startDay = value.clone().startOf('month').startOf('week');
+  const endDay = value.clone().endOf('month').endOf('week');
+  const day = startDay.clone().subtract(1, 'day');
+  const calendar = [];
+  return (
+    <div>
+      {startDay.format('MM/DD')} - {endDay.format('MM/DD')}
+    </div>
+  );
 }
