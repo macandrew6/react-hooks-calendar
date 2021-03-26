2b4859fc8ce43458d7d4ea9d0f8a946ef95035fb
macandrew6 <mr.betterandrewmac@gmail.com>
2021-03-25 23:19:03 -0700

    lays out curr calendar month

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from 'moment';
+import './styles.css';
 
 export default function Calendar() {
   const value = moment();
@@ -7,9 +8,23 @@ export default function Calendar() {
   const endDay = value.clone().endOf('month').endOf('week');
   const day = startDay.clone().subtract(1, 'day');
   const calendar = [];
+
+  while (day.isBefore(endDay, 'day')) {
+    calendar.push(
+      Array(7)
+        .fill(0)
+        .map(() => day.add(1, 'day').clone())
+    );
+  }
   return (
-    <div>
-      {startDay.format('MM/DD')} - {endDay.format('MM/DD')}
+    <div className='calendar'>
+      {calendar.map((week) => (
+        <div>
+          {week.map((day) => (
+            <div className='day'>{day.format('D').toString()}</div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
