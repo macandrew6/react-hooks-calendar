32b01ec951141dc62d625c1b3a6637e4210fb71a
macandrew6 <mr.betterandrewmac@gmail.com>
2021-03-29 17:43:54 -0700

    adding calendar hooks

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import './styles.css';
 
 export default function Calendar() {
-  const value = moment();
+  const [calendar, setCalendar] = useState([]);
+  const [value, setValue] = useState(moment());
+
   const startDay = value.clone().startOf('month').startOf('week');
   const endDay = value.clone().endOf('month').endOf('week');
   const day = startDay.clone().subtract(1, 'day');
-  const calendar = [];
 
   while (day.isBefore(endDay, 'day')) {
     calendar.push(
