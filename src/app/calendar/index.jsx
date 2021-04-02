0fd1383f9206bbc3a4f1642c4ab2e04f8127a336
macandrew6 <mr.betterandrewmac@gmail.com>
2021-04-01 18:35:47 -0700

    functioning next and previous month buttons

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -13,9 +13,35 @@ export default function Calendar() {
     setCalendar(buildCalendar(value));
   }, [value]);
 
+  function currMonthName() {
+    return value.format('MMMM');
+  }
+
+  function currYear() {
+    return value.format('YYYY');
+  }
+
+  function prevMonth() {
+    return value.clone().subtract(1, 'month');
+  }
+
+  function nextMonth() {
+    return value.clone().subtract(1, 'month');
+  }
+
   return (
     <div className='calendar'>
-      <div className='header'></div>
+      <div className='header'>
+        <div className='previous' onClick={() => setValue(prevMonth())}>
+          {String.fromCharCode(171)}
+        </div>
+        <div className='current'>
+          {currMonthName()} {currYear()}
+        </div>
+        <div className='next' onClick={() => setValue(nextMonth())}>
+          {String.fromCharCode(187)}
+        </div>
+      </div>
       <div className='body'>
         {calendar.map((week) => (
           <div>
